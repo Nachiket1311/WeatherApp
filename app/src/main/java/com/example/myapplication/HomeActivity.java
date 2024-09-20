2d5195954f70f5e4df0dcb0bade016a144b0b8f7
Nachiket Vaibhav Palekar <164024788+Nachiket1311@users.noreply.github.com>
2024-09-20 14:13:49 +0530

    main

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,10 +1,8 @@
 package com.example.myapplication;
 
-
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -16,7 +14,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.android.material.navigation.NavigationView;
-import android.R.layout.*;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -33,27 +30,34 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
+
         // Drawer layout and toggle setup
         drawerLayout = findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        // Navigation view setup
+        navigationView = findViewById(R.id.nav_view);
 
+        // Get the header view from the navigation view
+        View headerView = navigationView.getHeaderView(0);
+        TextView tvHeaderName = headerView.findViewById(R.id.tvHeaderName);
+        TextView tvHeaderEmail = headerView.findViewById(R.id.tvHeaderEmail);
 
+        // Get Firebase user info
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
+        if (currentUser != null) {
             String email = currentUser.getEmail();
             String displayName = currentUser.getDisplayName();
-            TextView v = (TextView) findViewById(R.id.tvHeaderName);
-            TextView x = (TextView) findViewById(R.id.tvHeaderEmail);
-            v.setText(displayName);
-            x.setText(email);
 
+            // Set the header text with user info
+            tvHeaderName.setText(displayName != null ? displayName : "No Name");
+            tvHeaderEmail.setText(email);
+        }
 
-        // Navigation view setup
-        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -72,7 +76,6 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             }
         });
-
     }
 
     @Override
