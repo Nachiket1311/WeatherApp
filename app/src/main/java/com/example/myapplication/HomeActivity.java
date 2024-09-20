4a1ffd1145bfd9025c0bd328783552d9bb2854dc
Nachiket Vaibhav Palekar <164024788+Nachiket1311@users.noreply.github.com>
2024-09-20 12:28:52 +0530

    main

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -5,6 +5,7 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -12,7 +13,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.android.material.navigation.NavigationView;
+import android.R.layout.*;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -34,6 +38,19 @@ public class HomeActivity extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+            String email = currentUser.getEmail();
+            String displayName = currentUser.getDisplayName();
+            TextView v = (TextView) findViewById(R.id.tvHeaderName);
+            TextView x = (TextView) findViewById(R.id.tvHeaderEmail);
+            v.setText(displayName);
+            x.setText(email);
+
 
         // Navigation view setup
         navigationView = findViewById(R.id.nav_view);
